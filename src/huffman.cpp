8375c2f30e1726d267bc612f79b53428feea108f
vyguzov.artem <vyguzov.artem@88935047-e299-4ba0-ae8b-38ca72f908ea>
2021-05-04 20:28:26 +0000

    Version 2 of hw_02

--- src/huffman.cpp
+++ src/huffman.cpp
@@ -1,58 +1,52 @@
 #include "huffman.h"
 #include <fstream>
 #include <climits>
+#include <algorithm>
 
 namespace huffman {
 
-    static int get_min(const std::vector<TreeNode *> &vec) {
-        int idx = 0;
-        for (int i = 0; i < vec.size(); i++) {
-            if (vec[i]->val < vec[idx]->val) {
-                idx = i;
-            }
-        }
-        return idx;
+    static int get_index_of_min_element(const std::vector<std::unique_ptr<TreeNode>> &nodes) {
+        return std::distance(nodes.begin(),
+                             std::min_element(nodes.begin(),
+                                              nodes.end(),
+                                              [](const std::unique_ptr<TreeNode> &a, const std::unique_ptr<TreeNode> &b) { return a->val <= b->val; }));
     }
 
-    HuffTree::HuffTree(const std::map<char, int> &freq) {
-        std::vector<TreeNode *> nodes;
-        try {
-            nodes.reserve(freq.size());
-            for (std::pair<char, int> tmp : freq) {
-                chars[tmp.first] = tmp.second;
-                nodes.push_back(new TreeNode(tmp.first, tmp.second));
+    HuffTree::HuffTree(const std::vector<int> &freq) {
+        std::vector<std::unique_ptr<TreeNode>> nodes;
+        chars.resize(freq.size());
+        for (int ch = 0; ch < freq.size(); ch++) {
+            chars[ch] = freq[ch];
+            if (freq[ch] != 0) {
+                size++;
+                nodes.push_back(std::make_unique<TreeNode>(ch, freq[ch]));
             }
-
-            size_t cur_size = nodes.size();
-            while (cur_size > 1) {
-                int idx = get_min(nodes);
-                std::swap(nodes[idx], nodes[cur_size - 1]);
-                TreeNode *min1 = nodes.back();
-                nodes.pop_back();
-                cur_size--;
-                idx = get_min(nodes);
-                std::swap(nodes[idx], nodes[cur_size - 1]);
-                TreeNode *min2 = nodes.back();
-                nodes.pop_back();
-                nodes.push_back(new TreeNode(min1, min2));
-            }
-
-            root = (nodes.empty()) ? nullptr : nodes[0];
-        } catch (std::bad_alloc &e) {
-            for (TreeNode *node : nodes) {
-                delete node;
-            }
-            throw e;
         }
+
+        size_t cur_size = nodes.size();
+        while (cur_size > 1) {
+            int idx = get_index_of_min_element(nodes);
+            std::swap(nodes[idx], nodes[cur_size - 1]);
+            std::unique_ptr<TreeNode> min1 = std::move(nodes.back());
+            nodes.pop_back();
+            cur_size--;
+            idx = get_index_of_min_element(nodes);
+            std::swap(nodes[idx], nodes[cur_size - 1]);
+            std::unique_ptr<TreeNode> min2 = std::move(nodes.back());
+            nodes.pop_back();
+            nodes.push_back(std::make_unique<TreeNode>(min1, min2));
+        }
+
+        root = (nodes.empty()) ? nullptr : std::move(nodes[0]);
     }
 
-    int HuffTree::decode(std::ifstream &in, std::ofstream &out, int cnt) {
-        int additionalData = (int)in.tellg();
-        TreeNode *cur = root;
+    int HuffTree::decode(std::ifstream &in, std::ofstream &out, int cnt) const {
+        int additionalData = in.tellg();
+        TreeNode *cur = root.get();
         if (cur == nullptr) return additionalData;
         if (cur->left == nullptr) {
             while (cnt != 0) {
-                out.write((char *) (&cur->ch), sizeof(char));
+                out.write((char *)&cur->ch, sizeof(char));
                 cnt--;
             }
         }
@@ -60,10 +54,10 @@ namespace huffman {
             uint8_t byte;
             in.read((char *)(&byte), sizeof(uint8_t));
             for (int i = CHAR_BIT - 1; i >= 0 && cnt != 0; i--) {
-                cur = (byte & (1 << i)) ? cur->right : cur->left;
+                cur = (byte & (1 << i)) ? cur->right.get() : cur->left.get();
                 if (cur->left == nullptr) {
-                    out.write((char *)(&cur->ch), sizeof(char));
-                    cur = root;
+                    out.write((char *)&cur->ch, sizeof(char));
+                    cur = root.get();
                     cnt--;
                 }
             }
@@ -71,56 +65,60 @@ namespace huffman {
         return additionalData;
     }
 
-    void HuffTree::archive(std::ofstream &out) {
-        uint8_t size = chars.size();
-        out.write((char *)(&size), sizeof(uint8_t));
-        for (std::pair<char, int> ch : chars) {
-            out.write(&ch.first, sizeof(char));
-            out.write((char *)(&ch.second), sizeof(int));
+    void HuffTree::archive(std::ofstream &out) const {
+        out.write((char *)(&size), sizeof(uint16_t));
+        for (int ch = 0; ch < chars.size(); ch++) {
+            if (chars[ch] != 0) {
+                out.write((char *)&ch, sizeof(uint8_t));
+                out.write((char *)&chars[ch], sizeof(int));
+            }
         }
     }
 
     void HuffmanArchiver::build_table(TreeNode *root, std::vector<bool> &cur_code) {
         if (root->left == nullptr) {
-            table[root->ch] = cur_code;
+            table[uint8_t(root->ch)] = cur_code;
             return;
         }
         cur_code.push_back(false);
-        build_table(root->left, cur_code);
+        build_table(root->left.get(), cur_code);
         cur_code.pop_back();
         cur_code.push_back(true);
-        build_table(root->right, cur_code);
+        build_table(root->right.get(), cur_code);
         cur_code.pop_back();
     }
 
     int HuffmanArchiver::zip(std::ifstream &in, std::ofstream &out) {
-        std::map<char, int> freq;
-        char ch;
+        std::vector<int> freq(1 << CHAR_BIT);
+        uint8_t ch;
         in.seekg(0, std::ifstream::end);
-        int number_of_chars = (int)in.tellg();
+        int number_of_chars = in.tellg();
         in.seekg(0, std::ifstream::beg);
         for (int i = 0; i < number_of_chars; i++) {
-            in.read(&ch, sizeof(char));
+            in.read((char *)&ch, sizeof(uint8_t));
             freq[ch]++;
         }
-        tree = new HuffTree(freq);
+        tree = std::make_unique<HuffTree>(freq);
         tree->archive(out);
-        int additionalData = (int)out.tellp();
+        int additionalData = out.tellp();
 
         in.seekg(0, std::ifstream::beg);
         TreeNode *cur = tree->get_root();
         std::vector<bool> cur_code;
         cur_code.reserve(1 << CHAR_BIT);
-        if (cur != nullptr) build_table(cur, cur_code);
+        if (cur != nullptr) {
+            table.resize(1 << CHAR_BIT);
+            build_table(cur, cur_code);
+        }
         uint8_t tmp;
         int cnt = 0;
         for (int i = 0; i < number_of_chars; i++) {
-            in.read(&ch, sizeof(char));
+            in.read((char *)&ch, sizeof(uint8_t));
             for (bool chr : table[ch]) {
                 cnt++;
                 tmp = 2 * tmp + chr;
                 if (cnt == CHAR_BIT) {
-                    out.write((char *)(&tmp), sizeof(uint8_t));
+                    out.write((char *)&tmp, sizeof(uint8_t));
                     tmp = 0;
                     cnt = 0;
                 }
@@ -132,24 +130,24 @@ namespace huffman {
             tmp *= 2;
             flag = true;
         }
-        if (flag) out.write((char *)(&tmp), sizeof(uint8_t));
+        if (flag) out.write((char *)&tmp, sizeof(uint8_t));
         return additionalData;
     }
 
     int HuffmanArchiver::unzip(std::ifstream &in, std::ofstream &out) {
-        uint8_t size;
-        in.read((char *) (&size), sizeof(uint8_t));
-        std::map<char, int> freq;
+        uint16_t size;
+        in.read((char *)&size, sizeof(uint16_t));
+        std::vector<int> freq(1 << CHAR_BIT);
         int sum = 0;
         for (int i = 0; i < size; i++) {
-            char ch;
+            uint8_t ch;
             int tmp;
-            in.read(&ch, sizeof(char));
-            in.read((char *) (&tmp), sizeof(int));
+            in.read((char *)&ch, sizeof(uint8_t));
+            in.read((char *)&tmp, sizeof(int));
             freq[ch] = tmp;
             sum += tmp;
         }
-        tree = new HuffTree(freq);
+        tree = std::make_unique<HuffTree>(freq);
         return tree->decode(in, out, sum);
     }
 
--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,6 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 #include "huffman.h"
-#include "additional.h"
 
 bool check_files(const std::string &filename1, const std::string &filename2) {
     std::ifstream in1(filename1);
@@ -22,14 +21,14 @@ bool check_files(const std::string &filename1, const std::string &filename2) {
 }
 
 TEST_CASE("huffman tree tests") {
-    std::map<char, int> freq;
+    std::vector<int> freq(1 << CHAR_BIT);
 
     SUBCASE("empty huffman tree test") {
         huffman::HuffTree tree = huffman::HuffTree(freq);
         CHECK_EQ(nullptr, tree.get_root());
     }
 
-    freq['a'] = 1;
+    freq[uint8_t('a')] = 1;
     SUBCASE("one symbol tree test") {
         huffman::HuffTree tree = huffman::HuffTree(freq);
         huffman::TreeNode *root = tree.get_root();
@@ -37,20 +36,20 @@ TEST_CASE("huffman tree tests") {
         CHECK_EQ(1, root->val);
     }
 
-    freq['b'] = 2;
-    freq['c'] = 4;
-    freq['d'] = 8;
+    freq[uint8_t('b')] = 2;
+    freq[uint8_t('c')] = 4;
+    freq[uint8_t('d')] = 8;
     SUBCASE("ordinary huffman tree test") {
         huffman::HuffTree tree = huffman::HuffTree(freq);
         huffman::TreeNode *cur_node = tree.get_root();
         CHECK_EQ(15, cur_node->val);
         CHECK_EQ('d', cur_node->right->ch);
         CHECK_EQ(8, cur_node->right->val);
-        cur_node = cur_node->left;
+        cur_node = cur_node->left.get();
         CHECK_EQ(7, cur_node->val);
         CHECK_EQ('c', cur_node->right->ch);
         CHECK_EQ(4, cur_node->right->val);
-        cur_node = cur_node->left;
+        cur_node = cur_node->left.get();
         CHECK_EQ(3, cur_node->val);
         CHECK_EQ('b', cur_node->right->ch);
         CHECK_EQ(2, cur_node->right->val);
@@ -64,7 +63,7 @@ TEST_CASE("huffman archiver tests") {
     huffman::HuffmanArchiver archiver, dearchiver;
 
     SUBCASE("zip and unzip an empty file") {
-        std::string empty_file = "../data/empty.txt";
+        std::string empty_file = "data/empty.txt";
         std::ifstream in(empty_file);
         std::ofstream out(zipped);
         int val1 = archiver.zip(in, out);
@@ -80,7 +79,7 @@ TEST_CASE("huffman archiver tests") {
     }
 
     SUBCASE("zip and unzip file with only one symbol") {
-        std::string one_symbol_file = "../data/one_symbol_file.txt";
+        std::string one_symbol_file = "data/one_symbol_file.txt";
         std::ifstream in(one_symbol_file);
         std::ofstream out(zipped);
         int val1 = archiver.zip(in, out);
@@ -96,7 +95,7 @@ TEST_CASE("huffman archiver tests") {
     }
 
     SUBCASE("zip and unzip an ordinary file") {
-        std::string ordinary_file = "../data/AStudyInScarlet.txt";
+        std::string ordinary_file = "data/AStudyInScarlet.txt";
         std::ifstream in(ordinary_file);
         std::ofstream out(zipped);
         int val1 = archiver.zip(in, out);
@@ -112,7 +111,7 @@ TEST_CASE("huffman archiver tests") {
     }
 
     SUBCASE("zip and unzip a bin file") {
-        std::string bin_file = "../data/file.bin";
+        std::string bin_file = "data/file.bin";
         std::ifstream in(bin_file);
         std::ofstream out(zipped);
         int val1 = archiver.zip(in, out);
@@ -125,35 +124,5 @@ TEST_CASE("huffman archiver tests") {
         fout.close();
         CHECK(check_files(bin_file, unzipped));
         CHECK_EQ(val1, val2);
-    }
-}
-
-TEST_CASE("parse arguments tests") {
-    SUBCASE("no arguments test") {
-        std::ifstream in;
-        std::ofstream out;
-        char *argv[0];
-        CHECK_THROWS(parse_arguments(0, argv, in, out));
-    }
-
-    SUBCASE("invalid arguments test") {
-        std::ifstream in;
-        std::ofstream out;
-        char *argv[] = {(char *)"-u", (char *)"-f"};
-        CHECK_THROWS(parse_arguments(2, argv, in, out));
-    }
-
-    SUBCASE("normal arguments test") {
-        std::ifstream in;
-        std::ofstream out;
-        char *argv[] = {(char *)"./hw_02", (char *)"-c", (char *)"-f", (char *)"test1.txt", (char *)"-o", (char *)"out.bin"};
-        CHECK_NOTHROW(parse_arguments(6, argv, in, out));
-    }
-
-    SUBCASE("different flags test") {
-        std::ifstream in;
-        std::ofstream out;
-        char *argv[] = {(char *)"./hw_02", (char *)"-u", (char *)"--file", (char *)"out.bin", (char *)"--output", (char *)"out.txt"};
-        CHECK_NOTHROW(parse_arguments(6, argv, in, out));
     }
 }
